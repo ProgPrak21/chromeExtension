569c507a784034e4852d0266439f777c1877bf09
flow <git@lowspace.de>
2021-05-11 01:16:10 +0200

    Fixed current tab injection

--- popup.js
+++ popup.js
@@ -1,28 +1,44 @@
+// not working yet
+function createUrl(url) {
+  return chrome.tabs.create({url}, function(tab) {
+    return tab, new Promise(resolve => {
+      chrome.tabs.onUpdated.addListener(function onUpdated(tabId, info) {
+        if (tab.id === tabId && info.status === 'complete') {
+          chrome.tabs.onUpdated.removeListener(onUpdated);
+          resolve();
+        }
+      });
+    });
+  });
+}
+
+function goToUrl(tab, url) {
+  chrome.tabs.update(tab.id, {url});
+  return new Promise(resolve => {
+    chrome.tabs.onUpdated.addListener(function onUpdated(tabId, info) {
+      if (tabId === tab.id && info.status === 'complete') {
+        chrome.tabs.onUpdated.removeListener(onUpdated);
+        resolve();
+      }
+    });
+  });
+}
 
 // Initialize button
 let issueRequest = document.getElementById("issueRequest");
 
-// When the button is clicked, inject clickPath on current page
+// When the button is clicked, 
+// inject connectors/com.instagram.js on instagram.com/download/request
 issueRequest.addEventListener("click", async () => {
-  // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  // lets open the page to request your data as new tab but don't make it active
+  // open the page to request the data in the current tab
   var requestURL = "https://www.instagram.com/download/request/";
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });  
   
-  let [reqTab] = await chrome.tabs.create({ url: requestURL, active: true }).then(() => {
-    const tabId = getTabId();
-    console.log(tabID);
-    chrome.scripting.executeScript({
-      code: `alert('injected');`,
-      target: { tabId: tabId },
-    });
+  await goToUrl(tab, requestURL);
+  //await createUrl(requestURL);
   
-  
-  const tabId1 = getTabId();
   chrome.scripting.executeScript({
-    target: { tabId: tabId1 },
-    file: 'connectors/com.instagram.js'
+    target: { tabId: tab.id },
+    files: ['connectors/com.instagram.js']
   });
-});
-  
 });
