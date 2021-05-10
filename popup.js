7e682537aaeb693656d5fd12c5899676d1bdac61
flow <git@lowspace.de>
2021-05-10 19:19:16 +0200

    broken

--- popup.js
+++ popup.js
@@ -1,28 +1,28 @@
-let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// Initialize button
+let issueRequest = document.getElementById("issueRequest");
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// When the button is clicked, inject clickPath on current page
+issueRequest.addEventListener("click", async () => {
+  // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+  // lets open the page to request your data as new tab but don't make it active
+  var requestURL = "https://www.instagram.com/download/request/";
+  
+  let [reqTab] = await chrome.tabs.create({ url: requestURL, active: true }).then(() => {
+    const tabId = getTabId();
+    console.log(tabID);
+    chrome.scripting.executeScript({
+      code: `alert('injected');`,
+      target: { tabId: tabId },
+    });
+  
+  
+  const tabId1 = getTabId();
   chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: clickSomething,
+    target: { tabId: tabId1 },
+    file: 'connectors/com.instagram.js'
   });
 });
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
-
-function clickSomething() {
-  let button = document.querySelector('[aria-label="Account"]');
-  button.click();
-}
+  
+});
